a55c996f5be85ba88b534c926e3d69806fa4ee52
Kateryna Derhaus <kdergaus@gmail.com>
2020-10-26 21:26:52 +0300

    Did everything except sorting blob values and the output itself

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -13,8 +13,8 @@ struct Blob
     int counter;
 };
 
-// int find_blob(struct Blob *blobs, int blobs_num, struct Point point);
-// int is_blob(struct Blob *blob, struct Point point);
+int any_point(struct Point *points, int amount);
+int are_compatible(struct Point point1, struct Point point2);
 
 //---------------------------------------------------------------------------------------------------------------------
 
@@ -24,6 +24,71 @@ int main()
     int rows, cols;
     scanf("%d %d", &rows, &cols);
 
+    struct Point *points_arr = (struct Point *)malloc(sizeof(struct Point) * rows*cols);
+    struct Blob working_blob;
+    working_blob.points = (struct Point *)malloc(sizeof(struct Point) * rows*cols);
+    working_blob.counter = 0;
+    int *blobs_values = (int *)malloc(sizeof(int) * (int)rows*cols/2);
+    int blob_counter = 0;
+    int point_counter = 0;
+    int flag = 0;
+
+    // input of points indexes
+    for (int i = 0;; ++i)
+    {
+        scanf("%d", &points_arr[i].row);
+        if (points_arr[i].row == -1)
+            break;
+        scanf("%d", &points_arr[i].col);
+
+        point_counter++;
+    }
+
+    // traversing points_arr until all points are eliminated
+    while (any_point(points_arr, point_counter))
+    {
+        flag = 1;
+        for (int i = 0; i < point_counter; ++i)
+        {
+            // skipping already used point
+            if (points_arr[i].row == -1)
+                continue;
+
+            // special case with the start of a new blob
+            if (working_blob.counter == 0)
+            {
+                working_blob.points[0].row = points_arr[i].row;
+                working_blob.points[0].col = points_arr[i].col;
+                working_blob.counter++;
+                points_arr[i].row = -1;
+                flag = 0;
+                break;
+            }
+
+            // check for compatibility
+            for (int j = 0; j < working_blob.counter; ++j)
+            {
+                if (are_compatible(points_arr[i], working_blob.points[j]))
+                {
+                    working_blob.points[0].row = points_arr[i].row;
+                    working_blob.points[0].col = points_arr[i].col;
+                    points_arr[i].row = -1;
+                    working_blob.counter++;
+                    flag = 0;
+                }
+            }
+        }
+
+        // all points of a blob are found
+        if (flag)
+        {
+            blobs_values[blob_counter++] = working_blob.counter;
+            working_blob.counter = 0;
+        }
+    }
+
+    // add the last blob from working_blob
+    blobs_values[blob_counter++] = working_blob.counter;
 
 
     return 0;
@@ -31,33 +96,20 @@ int main()
 
 //---------------------------------------------------------------------------------------------------------------------
 
-/*int find_blob(struct Blob *blobs, int blobs_num, struct Point point)
+int any_point(struct Point *points, int amount)
 {
-    for (int i = 0; i < blobs_num; ++i)
-        if (is_blob(&blobs[i], point))
-        {
-            printf("Blob index: %d\n", i); //delete
+    for(int i = 0; i < amount; ++i)
+        if (points[i].row != -1)
             return 1;
-        }
 
     return 0;
 }
 
-int is_blob(struct Blob *blob, struct Point point)
+int are_compatible(struct Point point1, struct Point point2)
 {
-    for (int i = 0; i < blob->counter; ++i)
-    {
-        if (point.row >= blob->points[i].row - 1 && point.row <= blob->points[i].row + 1 &&
-            point.col >= blob->points[i].col - 1 && point.col <= blob->points[i].col + 1)
-        {
-            blob->points[blob->counter] = point;
-            blob->counter++;
-            printf("Point: %d %d | ", blob->points[blob->counter-1].row, blob->points[blob->counter-1].col); // delete
-            printf("Current blob counter: %d | ", blob->counter); // delete
-            return 1;
-        }
-    }
+    if (point1.row >= point2.row - 1 && point1.row <= point2.row + 1 &&
+        point1.col >= point2.col - 1 && point1.col <= point2.col + 1)
+        return 1;
 
     return 0;
 }
-*/
