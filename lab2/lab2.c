af562f51309fc8fe81d30b9411fd64cdc6562a56
Kateryna Derhaus <kdergaus@gmail.com>
2020-10-26 18:31:45 +0300

    Since the developed was buggy, the main logic was deleted. Finished necessary structures

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Point
 {
@@ -8,8 +9,14 @@ struct Point
 
 struct Blob
 {
-    // point array and counter for those points
+    struct Point *points;
+    int counter;
 };
+
+// int find_blob(struct Blob *blobs, int blobs_num, struct Point point);
+// int is_blob(struct Blob *blob, struct Point point);
+
+//---------------------------------------------------------------------------------------------------------------------
 
 int main()
 {
@@ -17,15 +24,40 @@ int main()
     int rows, cols;
     scanf("%d %d", &rows, &cols);
 
-    struct Point input_point;
-
-    for (int i = 0;; ++i)
-    {
-        scanf("%d", &input_point.row);
-        if (input_point.row < 0) break;
-        scanf("%d", &input_point.col);
 
 
-    }
     return 0;
 }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+/*int find_blob(struct Blob *blobs, int blobs_num, struct Point point)
+{
+    for (int i = 0; i < blobs_num; ++i)
+        if (is_blob(&blobs[i], point))
+        {
+            printf("Blob index: %d\n", i); //delete
+            return 1;
+        }
+
+    return 0;
+}
+
+int is_blob(struct Blob *blob, struct Point point)
+{
+    for (int i = 0; i < blob->counter; ++i)
+    {
+        if (point.row >= blob->points[i].row - 1 && point.row <= blob->points[i].row + 1 &&
+            point.col >= blob->points[i].col - 1 && point.col <= blob->points[i].col + 1)
+        {
+            blob->points[blob->counter] = point;
+            blob->counter++;
+            printf("Point: %d %d | ", blob->points[blob->counter-1].row, blob->points[blob->counter-1].col); // delete
+            printf("Current blob counter: %d | ", blob->counter); // delete
+            return 1;
+        }
+    }
+
+    return 0;
+}
+*/
