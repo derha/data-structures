6a7016b5137a36c07cc99cb9bc9d48670570cb39
Kateryna Derhaus <kdergaus@gmail.com>
2020-11-06 14:18:32 +0300

    Added initial files for lab3

---
+++ lab3/function.h
@@ -0,0 +1,33 @@
+struct nodeFB
+{
+	int id;
+	int age;
+	struct nodeFB *next;
+};
+
+struct nodeGS
+{
+	int id;
+	struct nodeGS *next;
+};
+
+struct newNodeFB
+{
+	int id;
+	int age;
+	struct newNodeGS *next;
+};
+
+struct newNodeGS
+{
+	int id;
+	struct newNodeFB *next;
+};
+
+// You must write all the function definitions to be used in this lab into this file. 
+// You may also write other functions that may be called from our functions.
+// Do not make any changes to the main.c file.
+// Upload function.h file to the system as StudentNumber.h.
+
+
+
