cb39c27e987860d9e13ece45db1c859584984f53
Kateryna Derhaus <kdergaus@gmail.com>
2020-12-13 21:42:17 +0300

    Modified version with actual employee stack

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -6,7 +6,6 @@ struct employee
 {
     int id;
     int total_time;
-    int available;
     int until_available;
 };
 
@@ -29,21 +28,30 @@ struct report_node
 };
 
 void append_report_node(struct report_node **head, struct report_node **tail, struct report_node *new_node);
-void employees_update(struct employee employees[], int time_update);
+void employees_update(struct employee available_employees[], struct employee working_employees[],
+                      int *ae_top, int *we_top, int time_update);
 void print_report(struct report_node *head);
-void print_employees(struct employee employees[]);
+void print_employees(struct employee available_employees[], struct employee working_employees[], int ae_top, int we_top);
+void push(struct employee employees[], int *top, struct employee emp);
+struct employee pop(struct employee employees[], int *top);
+struct employee peek(struct employee employees[], int top);
 
+//---------------------------------------------------------------------------------------------------------------------
 
 int main()
 {
     // initializing *stack* of employees
-    struct employee employees[MAX_EMP];
+    struct employee available_employees[MAX_EMP];
+    struct employee working_employees[MAX_EMP];
+    int ae_top = -1;
+    int we_top = -1;
     for (int i = 0; i < MAX_EMP; ++i)
     {
-        employees[i].id = i + 1;
-        employees[i].total_time = 0;
-        employees[i].available = 1;
-        employees[i].until_available = 0;
+        struct employee emp;
+        emp.id = i + 1;
+        emp.total_time = 0;
+        emp.until_available = 0;
+        push(available_employees, &ae_top, emp);
     }
 
     // report/linked list
@@ -69,10 +77,10 @@ int main()
             new_customer.id = exit_check;
             scanf("%d %d", &new_customer.coming_time, &new_customer.processing_time);
 
-            if (new_customer.coming_time > current_time)
+            while (new_customer.coming_time > current_time)
             {
-                employees_update(employees, new_customer.coming_time - current_time);
-                current_time = new_customer.coming_time;
+                employees_update(available_employees, working_employees, &ae_top, &we_top,1);
+                current_time++;
             }
             new_customer.waiting_time = current_time - new_customer.coming_time;
 
@@ -80,48 +88,48 @@ int main()
                 current_time = new_customer.coming_time;
         }
 
-        // traversing employees from the end
-        for (int i = MAX_EMP - 1; i >= 0; --i)
+
+        // case when all employees are busy
+        if (ae_top == -1)
         {
-            // case when all employees are busy
-            if (i == 0 && !(employees[i].available))
-            {
-                new_customer.waiting_time += 1;
-                customer_input = 0;
-                employees_update(employees, 1);
-                current_time += 1;
-            }
+            new_customer.waiting_time += 1;
+            customer_input = 0;
+            employees_update(available_employees, working_employees, &ae_top, &we_top, 1);
+            current_time += 1;
+        }
 
-            if (employees[i].available)
-            {
-                // creating new report line
-                struct report_node *node = (struct report_node *)malloc(sizeof(struct report_node));
-                node->customer_id = new_customer.id;
-                node->employee_id = employees[i].id;
-                node->start = current_time;
-                node->finish = current_time + new_customer.processing_time;
-                node->customer_waiting = new_customer.waiting_time;
-                node->next = NULL;
-                append_report_node(&report, &end, node);
+        else
+        {
+            struct employee emp = peek(available_employees, ae_top);
 
-                // updating taken employee's availability
-                employees[i].total_time += new_customer.processing_time;
-                employees[i].available = 0;
-                employees[i].until_available = new_customer.processing_time;
+            // creating new report line
+            struct report_node *node = (struct report_node *)malloc(sizeof(struct report_node));
+            node->customer_id = new_customer.id;
+            node->employee_id = emp.id;
+            node->start = current_time;
+            node->finish = current_time + new_customer.processing_time;
+            node->customer_waiting = new_customer.waiting_time;
+            node->next = NULL;
+            append_report_node(&report, &end, node);
 
-                customer_input = 1;
-                break;
-            }
+            // updating taken employee's availability
+            emp.total_time += new_customer.processing_time;
+            emp.until_available = new_customer.processing_time;
+            push(working_employees, &we_top, emp);
+            pop(available_employees, &ae_top);
+
+            customer_input = 1;
         }
     }
 
     print_report(report);
     printf("\n");
-    print_employees(employees);
+    print_employees(available_employees, working_employees, ae_top, we_top);
 
     return 0;
 }
 
+//---------------------------------------------------------------------------------------------------------------------
 
 void append_report_node(struct report_node **head, struct report_node **tail, struct report_node *new_node)
 {
@@ -136,15 +144,18 @@ void append_report_node(struct report_node **head, struct report_node **tail, st
     *tail = new_node;
 }
 
-void employees_update(struct employee employees[], int time_update)
+void employees_update(struct employee available_employees[], struct employee working_employees[],
+                      int *ae_top, int *we_top, int time_update)
 {
-    for (int i = 0; i < MAX_EMP; ++i)
+    for (int i = *we_top; i >= 0; --i)
     {
-        if (!(employees[i].available))
+        working_employees[i].until_available -= time_update;
+        if (working_employees[i].until_available <= 0)
         {
-            employees[i].until_available -= time_update;
-            if (employees[i].until_available <= 0)
-                employees[i].available = 1;
+            push(available_employees, ae_top, working_employees[i]);
+            for (int j = i; j < *we_top; ++j)
+                working_employees[j] = working_employees[j + 1];
+            (*we_top)--;
         }
     }
 }
@@ -159,8 +170,40 @@ void print_report(struct report_node *head)
     }
 }
 
-void print_employees(struct employee employees[])
+void print_employees(struct employee available_employees[], struct employee working_employees[], int ae_top, int we_top)
 {
+    int employees[MAX_EMP][2];
+
+    for (int i = 0; i <= ae_top; ++i)
+    {
+        employees[available_employees[i].id - 1][0] = available_employees[i].id;
+        employees[available_employees[i].id - 1][1] = available_employees[i].total_time;
+    }
+
+    for (int i = 0; i <= we_top; ++i)
+    {
+        employees[working_employees[i].id - 1][0] = working_employees[i].id;
+        employees[working_employees[i].id - 1][1] = working_employees[i].total_time;
+    }
+
     for (int i = MAX_EMP - 1; i >= 0; --i)
-        printf("%d %d\n", employees[i].id, employees[i].total_time);
+        printf("%d %d\n", employees[i][0], employees[i][1]);
+}
+
+void push(struct employee employees[], int *top, struct employee emp)
+{
+    (*top)++;
+    employees[*top] = emp;
+}
+
+struct employee pop(struct employee employees[], int *top)
+{
+    struct employee emp = employees[*top];
+    (*top)--;
+    return emp;
+}
+
+struct employee peek(struct employee employees[], int top)
+{
+    return employees[top];
 }
