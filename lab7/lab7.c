581c9e3683d35c517b840b0ae135706b53622fd8
Kateryna Derhaus <kdergaus@gmail.com>
2020-12-27 20:05:51 +0300

    Made a version with normal binary search tree

---
+++ lab7/lab7.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int num;
+    int balance;
+    struct node *left_child;
+    struct node *right_child;
+};
+
+void insert(struct node **root, struct node *new_node);
+void print_nums(struct node *root);
+void print_tree(struct node *root);
+int print_level(struct node *root, int until_stop);
+
+//----------------------------------------------------------------------------------------------------------------------
+
+int main()
+{
+    struct node *root = NULL;
+
+    // input and AVL tree creation
+    int exit_check;
+    while (1)
+    {
+        scanf("%d", &exit_check);
+        if (exit_check == -1)
+            break;
+
+        struct node *new_node = (struct node *)malloc(sizeof(struct node));
+        new_node->num = exit_check;
+        new_node->balance = 0;
+        new_node->left_child = NULL;
+        new_node->right_child = NULL;
+
+        insert(&root, new_node);
+    }
+
+    print_nums(root);
+    printf("\n");
+    print_tree(root);
+
+    return 0;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+void insert(struct node **root, struct node *new_node)
+{
+    if (!(*root))
+    {
+        *root = new_node;
+        return;
+    }
+
+    if (new_node->num < (*root)->num)
+        insert(&((*root)->left_child), new_node);
+    else
+        insert(&((*root)->right_child), new_node);
+}
+
+void print_nums(struct node *root)
+{
+    if (root->left_child != NULL)
+        print_nums(root->left_child);
+
+    printf("%d\n", root->num);
+
+    if (root->right_child != NULL)
+        print_nums(root->right_child);
+
+    return;
+}
+
+void print_tree(struct node *root)
+{
+    printf("%d (%d B)\n", root->num, root->balance);
+
+    for (int i = 0; ; ++i)
+    {
+        if (!print_level(root, i))
+            break;
+        printf("\n");
+    }
+}
+
+int print_level(struct node *root, int until_stop)
+{
+    if (!until_stop)
+    {
+        if (root->left_child != NULL)
+            printf("%d (%d L) (%d B) ", root->left_child->num, root->num, root->left_child->balance);
+
+        if (root->right_child != NULL)
+            printf("%d (%d R) (%d B) ", root->right_child->num, root->num, root->right_child->balance);
+
+        return 1;
+    }
+
+    int flag = 0;
+
+    if (root->left_child != NULL)
+        if (print_level(root->left_child, until_stop - 1))
+            flag = 1;
+
+    if (root->right_child != NULL)
+        if (print_level(root->right_child, until_stop - 1))
+            flag = 1;
+
+    return flag;
+}
